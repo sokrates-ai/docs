e818e8ddb19e9be4c2b6a87222594ec0998b4d29
Mik Müller <83001409+MikMuellerDev@users.noreply.github.com>
2024-11-24 18:07:25 +0100

    feat: Add GitHub actions deployment

--- next.config.js
+++ next.config.js
@@ -1,9 +1,20 @@
+const nextConfig = {
+  reactStrictMode: true,
+  distDir: 'dist',
+  // Export output
+  output: "export",
+  images: {
+      unoptimized: true
+  },
+  basePath: '/docs',
+}
+
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.jsx',
 })
- 
-module.exports = withNextra()
- 
+
+module.exports = withNextra(nextConfig)
+
 // If you have other Next.js configurations, you can pass them as the parameter:
 // module.exports = withNextra({ /* other next.js config */ })
