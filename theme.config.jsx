f80dba5ac9e9eeb1f72181d33544c716817040b9
Mik Müller <83001409+MikMuellerDev@users.noreply.github.com>
2024-11-24 20:07:44 +0100

    chore: Rename LearnHouse -> Sokrates

--- theme.config.jsx
+++ theme.config.jsx
@@ -3,17 +3,17 @@ import Logo from "./components/Logo/Logo";
 export default {
   logo: <Logo />,
   project: {
-    link: "https://github.com/learnhouse/learnhouse",
+    link: "https://github.com/sokrates-ai/platform",
   },
-  docsRepositoryBase: "https://github.com/learnhouse/docs/tree/main",
+  docsRepositoryBase: "https://github.com/sokrates-ai/docs",
   chat: {
-    link: "https://discord.gg/CMyZjjYZ6x",
+    link: "tyron.franzke@student.hpi.uni-potsdam.de",
   },
 
   head: (
     <>
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <meta property="og:title" content="LearnHouse Docs" />
+      <meta property="og:title" content="Sokrates Docs" />
       <meta property="og:description" content="The next-gen open source learning software" />
       <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
       <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
@@ -22,35 +22,30 @@ export default {
     </>
   ),
   footer: {
-    text: <span>{new Date().getFullYear()} © LearnHouse.</span>,
+    text: <span>{new Date().getFullYear()} © Sokrates.</span>,
   },
   sidebar: {
     defaultMenuCollapseLevel: 2,
   },
   useNextSeoProps() {
     return {
-      titleTemplate: "%s – LearnHouse Docs",
-      canonical: "https://docs.learnhouse.app",
+      titleTemplate: "%s – Sokrates Docs",
+      canonical: "https://sokrates-ai.github.io/docs/",
       description: "The next-gen open source learning software",
 
       openGraph: {
         type: "website",
         locale: "en_US",
-        url: "https://docs.learnhouse.app",
-        site_name: "LearnHouse Docs",
+        url: "https://sokrates-ai.github.io/docs/",
+        site_name: "Sokrates Docs",
         images: [
           {
-            url: "https://docs.learnhouse.app/img/og.png",
-            alt: "LearnHouse Docs",
+            url: "https://sokrates-ai.github.io/docs/img/og.png",
+            alt: "Sokrates Docs",
             width: 1512,
             height: 687,
           },
         ],
-      },
-      twitter: {
-        handle: "@getlearnhouse",
-        site: "@getlearnhouse",
-        cardType: "summary_large_image",
       },
     };
   },
